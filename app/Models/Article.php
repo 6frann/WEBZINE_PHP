2208488d359534c223f387821e054c016ddac610
Francis Senga-Vita <148126721+6frann@users.noreply.github.com>
2025-05-20 14:05:54 +0200

    Feat#4 création des endpoints  (#53)
    * .Ds_Store
    
    * Mise en place du controller et de la route pour les articles
    
    * Mise en place de la route détail d'un article et de sa fonction
    
    * Ajout des relations entre les modèles Article, Category, Author, Tag
    
    * Controller et route de la liste des videos ainsi que détail d'une video

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'article_has_tag');
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    use HasFactory;
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class, 'article_has_tag');
+    }
 }
