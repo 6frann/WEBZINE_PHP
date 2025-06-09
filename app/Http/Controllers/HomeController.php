ab1db40d204f81b3995d840b8961f087c7da58ed
francis <sixfranc7@gmail.com>
2025-06-09 16:24:23 +0200

    affichage de category sur topArticle et article

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,8 @@ class HomeController extends Controller
 {
     public function index() {
         return view('pages.home', [
-            'topArticles' => Article::with('category', 'tags', 'author')->latest()->take(1)->get(),
-            'articles' => Article::with('category', 'tags', 'author')->latest()->take(4)->get(),
+            'topArticles' => Article::with('category')->latest()->take(1)->get(),
+            'articles' => Article::with('category', 'author')->latest()->take(4)->get(),
             'videos' => Video::latest()->take(4)->get(),
         ]);
     }
