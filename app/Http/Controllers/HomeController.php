75046f88b38ab6aa17d91ab48691535e59a44e17
francis <sixfranc7@gmail.com>
2025-06-11 16:41:52 +0200

    Amélioration de l'affichage des articles dans home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
     public function index() {
         return view('pages.home', [
             'topArticles' => Article::with('category')->latest()->take(3)->get(),
-            'articles' => Article::with('category', 'author')->latest()->take(4)->get(),
+            'articles' => Article::with('category', 'author')->latest()->skip(3)->take(4)->get(),
             'videos' => Video::latest()->take(4)->get(),
         ]);
     }
