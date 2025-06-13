af21b8cd68af1dade20af5401a4df2132a72349a
francis <sixfranc7@gmail.com>
2025-06-13 20:46:53 +0200

    btn hamburger

--- resources/views/templates/home.blade.php
+++ resources/views/templates/home.blade.php
@@ -31,6 +31,11 @@
                 </button>
             </form>
         </div>
+        {{-- <button class="hamburger p-2 focus:outline-none" onclick="toggleMobileMenu()">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+            </svg>
+        </button> --}}
         
         {{-- nav --}}
         @include('templates.partials._nav')
--- resources/views/templates/partials/_header.blade.php
+++ resources/views/templates/partials/_header.blade.php
@@ -48,11 +48,11 @@
                             </svg>
                         </button>
                 </div>
-                {{-- <button class="hamburger p-2 focus:outline-none" onclick="toggleMobileMenu()">
+                <button class="hamburger p-2 focus:outline-none" onclick="toggleMobileMenu()">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
                     </svg>
-                </button> --}}
+                </button>
             </div>
         </div>
     </div>
